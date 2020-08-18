db7dd388c5c8898a321d6750250ff507d5087ae2
Bernardo Chagas <bernardochagas@usp.br>
2020-08-18 13:21:01 -0300

    Isolate main behavior in own function

--- src/wonka/core.clj
+++ src/wonka/core.clj
@@ -5,41 +5,39 @@
   (with-open [reader (clojure.java.io/reader "./resources/input.txt")]
     (reduce conj [] (line-seq reader))))
 
-(def heights
-  (vec (map (fn [i] (Integer/parseInt i)) (getContent))))
+(defn getHeights [content]
+  (vec (map (fn [i] (Integer/parseInt i)) content)))
 
-(def indexed
-  (map-indexed
-    (fn [idx itm] [itm idx]) heights))
+(defn calcChoc [heights]
+  (let
+    [indexed (map-indexed (fn [idx itm] [itm idx]) heights)
+     peaks (vec (filter (fn [[itm]] (> itm 0)) indexed))
+     lastPeak (second (last peaks))
 
-(def peaks
-  (vec (filter (fn [[itm]] (> itm 0)) indexed)))
+     streak
+     (fn [i k acc]
+       (letfn [(endOfStreak [] (or (>= k lastPeak) (>= (nth heights k) (nth heights i))))
+               (streakHeight [] (* (min (nth heights i) (nth heights k)) (count acc)))]
+         (if (endOfStreak)
+           (list (reduce - (streakHeight) acc) k)
+           (recur i (+ k 1) (conj acc (nth heights k))))))
 
-(def lastPeak (second (last peaks)))
+     iter
+     (fn [i, total]
+       (letfn [(endOfWalls [] (>= i (- (count heights) 1)))
+               (streakFromI [] (streak i (+ i 1) (vector)))
+               (isPeak [] (> (nth heights i) 0))]
+         (if (endOfWalls)
+           total
+           (if (isPeak)
+             (let [[result streakEndIndex] (streakFromI)]
+               (recur streakEndIndex (+ total result)))
+             (recur (+ i 1) total)))))]
 
-(defn streak
-  [i k acc]
-  (letfn
-    [(endOfStreak [] (or (>= k lastPeak) (>= (nth heights k) (nth heights i))))
-     (streakHeight [] (* (min (nth heights i) (nth heights k)) (count acc)))]
-    (if (endOfStreak)
-      (list (reduce - (streakHeight) acc) k)
-      (recur i (+ k 1) (conj acc (nth heights k))))))
-
-(defn iter [i, total]
-  (letfn
-    [(endOfWalls [] (>= i (- (count heights) 1)))
-     (streakFromI [] (streak i (+ i 1) (vector)))
-     (isPeak [] (> (nth heights i) 0))]
-    (if (endOfWalls)
-      total
-      (if (isPeak)
-        (let [[result streakEndIndex] (streakFromI)]
-          (recur streakEndIndex (+ total result)))
-        (recur (+ i 1) total)))))
+    (iter 0 0)))
 
 (defn -main
   "Let's see what this thingy does"
   [& args]
-  (println (iter 0 0)))
+  (println (calcChoc (getHeights (getContent)))))
 
