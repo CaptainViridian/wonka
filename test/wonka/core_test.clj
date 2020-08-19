8b18c5c71c56dd10f86e6dc9d8822742e47c199d
Bernardo Chagas <bernardochagas@usp.br>
2020-08-19 19:07:04 -0300

    Fix assertions

--- test/wonka/core_test.clj
+++ test/wonka/core_test.clj
@@ -2,60 +2,65 @@
   (:require [clojure.test :refer :all]
             [wonka.core :refer :all]))
 
-(def smallList1 [(list 0 2 0 0 2 0) 4])
+(def smallList1 (list 0 2 0 0 2 0))
 
 (deftest getHeights-test
-  (testing "Receives a list of strings and returns a list of heights (numbers)")
-  (is (= (first smallList1) (getHeights (getContent "./resources/tiny.txt")))))
+  (testing "Receives a list of strings and returns a list of heights (numbers)"
+    (is (= smallList1 (getHeights (getContent "./resources/tiny.txt"))))))
 
-(def emptyList [(list) 0])
-(def smallList2 [(list 0 2 0 0 1 0) 2])
-(def smallList3 [(list 0 1 2 0 2 0) 2])
-(def smallList4 [(list 0 2 1 0 2 0) 3])
-(def slightlyBiggerList [(getHeights (getContent "./resources/small.txt")) 12])
+(def emptyList (list))
+(def smallList2 (list 0 2 0 0 1 0))
+(def smallList3 (list 0 1 2 0 2 0))
+(def smallList4 (list 0 2 1 0 2 0))
+(def slightlyBiggerList (getHeights (getContent "./resources/small.txt")))
 (def mediumList (getHeights (getContent "./resources/medium.txt")))
 (def hugeList (getHeights (getContent "./resources/large.txt")))
-(def increasingList [(list 1 2 3 4 5 6 70) 0])
-(def decreasingList [(reverse (first increasingList)) 0])
+(def increasingList (list 1 2 3 4 5 6 70))
+(def decreasingList (reverse increasingList))
+(def steadyList (list 4 4 4 4 4 4 4))
 
 (deftest calcChoc-empty-list
-  (testing "Returns 0 for empty list")
-  (is (= (second emptyList) (calcChoc (first emptyList)))))
+  (testing "Returns 0 for empty list"
+    (is (= 0 (calcChoc emptyList)))))
 
 (deftest calcChoc-small-list-1
-  (testing "Returns 4 for small list 1")
-  (is (= (second smallList1) (calcChoc (first smallList1)))))
+  (testing "Returns 4 for small list 1"
+    (is (= 4 (calcChoc smallList1)))))
 
 (deftest calcChoc-small-list-2
-  (testing "Returns 2 for small list 2")
-  (is (= (second smallList2) (calcChoc (first smallList2)))))
+  (testing "Returns 2 for small list 2"
+    (is (= 2 (calcChoc smallList2)))))
 
 (deftest calcChoc-small-list-3
-  (testing "Returns 2 for small list 3")
-  (is (= (second smallList3) (calcChoc (first smallList3)))))
+  (testing "Returns 2 for small list 3"
+    (is (= 2 (calcChoc smallList3)))))
 
 (deftest calcChoc-small-list-4
-  (testing "Returns 3 for small list 4")
-  (is (= (second smallList4) (calcChoc (first smallList4)))))
+  (testing "Returns 3 for small list 4"
+    (is (= 3 (calcChoc smallList4)))))
 
 (deftest calcChoc-not-so-small-list
-  (testing "Returns 12 for slightly bigger list")
-  (is (= (second slightlyBiggerList) (calcChoc (first slightlyBiggerList)))))
+  (testing "Returns 12 for slightly bigger list"
+    (is (= 12 (calcChoc slightlyBiggerList)))))
 
 (deftest calcChoc-medium-list
-  (testing "Does not Return 0 for medium list")
-  (is (not (= 0 (calcChoc mediumList)))))
+  (testing "Does not Return 0 for medium list"
+    (is (not (= 0 (calcChoc mediumList))))))
 
 (deftest calcChoc-huge-list
-  (testing "Does not Overflow Stack for huge list")
-  (calcChoc hugeList))
+  (testing "Does not Overflow Stack for huge list"
+    (calcChoc hugeList)))
 
 (deftest calcChoc-increasing-list
-  (testing "Returns 0 for increasing-only list")
-  (is (= (second increasingList) (calcChoc (first increasingList)))))
+  (testing "Returns 0 for increasing-only list"
+    (is (= 0 (calcChoc increasingList)))))
 
 (deftest calcChoc-decreasing-list
-  (testing "Returns 0 for decreasing-only list")
-  (is (= (second decreasingList) (calcChoc (first decreasingList)))))
+  (testing "Returns 0 for decreasing-only list"
+    (is (= 0 (calcChoc decreasingList)))))
+
+(deftest calcChoc-steady-list
+  (testing "Returns 0 for list with all the same values"
+    (is (= 0 (calcChoc steadyList)))))
 
 (run-tests 'wonka.core-test)
